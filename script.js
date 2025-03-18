4d0ac8d82c1e938bb675a1390dec3aa6bfea3480
Roman95687 <rufg1793@gmail.com>
2025-03-18 08:27:12 +0000

    Добавлен эффект анимации текста с сердцем

--- script.js
+++ script.js
@@ -10,9 +10,3 @@ document.addEventListener("DOMContentLoaded", () => {
         heart.classList.add("show-heart"); // Показываем сердце
     }, 1500);
 });
-git init
-git add .
-git commit -m "Добавлен эффект анимации текста с сердцем"
-git branch -M main
-git remote add origin https://github.com/Roman95687/546464.git
-git push -u origin main
